eb8983361ad23583ed98f16402d821c9e929aec7
atanas <tachev_a@yahoo.com>
2021-10-07 16:04:06 +0300

    Initial Commit

---
+++ some_recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  some_recipes
+//
+//  Created by atanastachev on 7.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
